0f935366b4e576ab5fc7aaa9acd5bdcd0158fca7
bukita <choate1999@hotmail.com>
2022-04-01 12:10:18 +0800

    add information push and user management
    添加用户管理和内容推送

--- Assets/Scripts/MainSceneUIController.cs
+++ Assets/Scripts/MainSceneUIController.cs
@@ -11,7 +11,7 @@ public class MainSceneUIController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame //
     void Update()
     {
         
